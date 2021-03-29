79aae268a334739d8417762206ee4b3ae80b7e24
hexing <hexing@scyuelai.com>
2021-03-29 15:07:14 +0800

    添加jaeger

--- demo-cli/go.mod
+++ demo-cli/go.mod
@@ -2,4 +2,8 @@ module github.com/hongxuandaozun/laracom/demo-cli
 
 go 1.14
 
-require github.com/micro/go-micro v1.18.0
+require (
+	github.com/hongxuandaozun/laracom/demo-service v0.0.0-20210329054807-5d6dae295438
+	github.com/micro/go-micro v1.18.0
+	github.com/micro/go-plugins/wrapper/trace/opentracing v0.0.0-20200119172437-4fe21aa238fd
+)
--- demo-cli/main.go
+++ demo-cli/main.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"context"
-	pb "github.com/hongxuandaozun/laracom/demo-service/demo"
+	pb "github.com/hongxuandaozun/laracom/demo-service/proto/demo"
+	"github.com/hongxuandaozun/laracom/demo-service/trace"
 	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/metadata"
+	traceplugin "github.com/micro/go-plugins/wrapper/trace/opentracing"
+	"github.com/opentracing/opentracing-go"
 	"log"
+	"os"
 )
 
 func main() {
-	service := micro.NewService(micro.Name("laracom.demo.cli"))
+	tracer, closer, err := trace.NewTracer("laracom.demo.cli", os.Getenv("MICRO_TRACE_SERVER"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closer.Close()
+
+	service := micro.NewService(micro.Name("laracom.demo.cli"), micro.WrapClient(traceplugin.NewClientWrapper(tracer)))
+
 	service.Init()
 
 	client := pb.NewDemoServiceClient("laracom.demo.service", service.Client())
-	rsp, err := client.SayHello(context.TODO(), &pb.DemoRequest{Name: "学院君"})
+
+	// 创建空的上下文,生成追踪span
+	span, ctx := opentracing.StartSpanFromContext(context.Background(), "call")
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		md = make(map[string]string)
+	}
+	defer span.Finish()
+
+	// 注入opentracing textmap 到空的上下文用于追踪
+	opentracing.GlobalTracer().Inject(span.Context(), opentracing.TextMap, opentracing.TextMapCarrier(md))
+	metadata.NewContext(opentracing.ContextWithSpan(ctx, span), md)
+	rsp, err := client.SayHello(ctx, &pb.DemoRequest{Name: "学院君"})
 	if err != nil {
 		log.Fatalf("服务调用失败：%v", err)
 		return
